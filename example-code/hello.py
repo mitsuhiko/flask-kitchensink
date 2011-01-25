1e447e91dca8799a11196e6b5059053616c002c5
Armin Ronacher <armin.ronacher@active-4.com>
2011-01-25 16:21:02 +0100

    Flask-Kitchensink now also sets up flashing messages and a development secret key.

--- example-code/hello.py
+++ example-code/hello.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
+app.secret_key = 'development secret key'
 app.debug = True
 
 
 @app.route('/')
 def index():
+    flash('Flask running and flashing messages works!')
     return render_template('index.html')
